663844eb84b790e5233a2ae8c78cd38a1998766e
Your Name <you@example.com>
2026-08-16 17:40:37 +0200

    install.lua first version

---
+++ install.lua
@@ -0,0 +1,33 @@
+-- Pastebin install script for CCLuaProject
+-- Constants
+dir_name = "libraries"
+update_url = "https://raw.githubusercontent.com/Vladexus7/CCLuaProject/main/update.lua"
+
+-- Functions
+
+local function check_install()
+    -- Check if the CCLuaProject is installed
+    if fs.exists(dir_name) then
+        return true
+    else
+        return false
+    end
+end
+
+local function install()
+    update = http.get(update_url).readAll()
+    local update_file = fs.open(dir_name .. "/update.lua", "w")
+    update_file.write(update)
+    fs.close(update_file)
+    print("Fresh install of CCLuaProject...")
+    shell.run(dir_name .. "/update.lua", "update")
+end
+
+local function main()
+    if not check_install() then
+        install()
+    else
+        print("CCLuaProject is already installed.")
+    end
+end
+main()
